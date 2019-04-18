08ff015382325de1f36541af93d363a5cfcfc25e
nichia <nichia@gmail.com>
2019-04-18 18:40:13 -0400

    Done.

--- reducer.js
+++ reducer.js
@@ -1,4 +1,20 @@
-let state;
+function createStore(reducer) {
+  let state;
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+
+  function getState() {
+    return state;
+  }
+
+  return {
+    dispatch,
+    getState
+  };
+}
 
 function changeCount(state = { count: 0 }, action) {
   switch (action.type) {
@@ -8,21 +24,17 @@ function changeCount(state = { count: 0 }, action) {
     default:
       return state;
   }
-};
-
-function dispatch(action){
-  state = changeCount(state, action);
-  render();
-};
+}
 
 function render() {
   let container = document.getElementById('container');
-  container.textContent = state.count;
-};
+  container.textContent = store.getState().count;
+}
 
-dispatch({ type: '@@INIT' })
+let store = createStore(changeCount); // createStore takes the changeCount reducer as an argument
+store.dispatch({ type: '@@INIT' });
 let button = document.getElementById('button');
 
 button.addEventListener('click', function() {
-    dispatch({ type: 'INCREASE_COUNT' });
-})
+  store.dispatch({ type: 'INCREASE_COUNT' });
+});
